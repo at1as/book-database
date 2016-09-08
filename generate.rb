a4caabafac70178e8eb60fd15b02798c23ba91fa
Jason Willems <jason@willems.ca>
2016-09-07 23:29:02 -0700

    Initial commit

---
+++ generate.rb
@@ -0,0 +1,20 @@
+require 'liquid'
+
+# Acquire list of books in directory
+saved_books_dir = "/Volumes/5TB-Media/Books/"
+Dir.chdir(saved_books_dir)
+book_list = Dir.glob('*').reject {|f| File.directory? f }
+
+
+# Map book title to list of formats
+books_and_formats = book_list.group_by { |x| x.rpartition('.')[0] }.map{ |k, v| [k, v.map{ |j| j.split('.').last }] }.to_h
+
+
+# Generate HTML page
+Dir.chdir(__dir__)
+text = File.read('books.liquid')
+@template = Liquid::Template.parse(text)
+output = @template.render('books' => books_and_formats, 'books_dir' => saved_books_dir)
+
+File.open('book-list.html', 'w') { |file| file.write(output) }
+
