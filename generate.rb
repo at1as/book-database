3a410f98e969eacfb6e29538ca50e3efecebc1c7
Jason Willems <jason@willems.ca>
2016-10-02 22:24:07 -0700

    Add year column to table

--- generate.rb
+++ generate.rb
@@ -1,20 +1,38 @@
 require 'liquid'
 
 # Acquire list of books in directory
-saved_books_dir = "./Books/"
+saved_books_dir = "/Volumes/5TB-MEDIA/Books/"
 Dir.chdir(saved_books_dir)
 book_list = Dir.glob('*').reject {|f| File.directory? f }
 
 
-# Map book title to list of formats
-books_and_formats = book_list.group_by { |x| x.rpartition('.')[0] }.map{ |k, v| [k, v.map{ |j| j.split('.').last }] }.to_h
+
+def strip_trailing_year(str)
+  # "Author – Title (YYYY)" => "Author - Title""
+  (title = /(.*)(\([0-9]{4}\)\Z)/.match(str)) ? title[1].strip : str.strip
+end
+
+
+detailed_book_list = book_list.group_by { |filename| 
+  filename.rpartition('.').first
+}.map do |k, v| 
+  [
+    k, 
+    {
+      "title"   => strip_trailing_year(k.partition(' - ').last),
+      "formats" => v.map { |j| j.split('.').last },
+      "authors" => k.split(' - ').first.strip,
+      "year"    => (k.rpartition('(').last.chomp(')') if k[-6..-1] =~ /([0-9]{4})/)
+    }
+  ] 
+end.to_h 
 
 
 # Generate HTML page
 Dir.chdir(__dir__)
 text = File.read('books.liquid')
 @template = Liquid::Template.parse(text)
-output = @template.render('books' => books_and_formats, 'books_dir' => saved_books_dir)
+output = @template.render('books' => detailed_book_list, 'books_dir' => saved_books_dir)
 
 File.open('book-list.html', 'w') { |file| file.write(output) }
 
